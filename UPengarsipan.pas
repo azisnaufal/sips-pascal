8721b116d16f16d4025ffda9df018518fe5fde99
alif ramdhan kusaeri <kusaerialif04@gmail.com>
2019-02-05 15:47:37 +0700

    nambah uses

--- UPengarsipan.pas
+++ UPengarsipan.pas
@@ -4,11 +4,13 @@ begin
     writeln('coba');
     
     //alif
-    writeln('Program Pengarsipan Surat');
-    writeln('Nama surat : ');
-    writeln('Tanggal    : ');
-    writeln('Pengirim   : ');
-    writeln('Penerima   : ');
+    uses crt, sysutils;
+      writeln('Program Pengarsipan Surat');
+      writeln('Nama surat : ');
+      writeln('Tanggal    : ');
+      writeln('Pengirim   : ');
+      writeln('Penerima   : ');
+
     //yusuf
     writeln('Yusuf');
     writeln('alif cabul ');
